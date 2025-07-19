8db1ebad87e38edcf4fe6ab9542481f9bc3a5090
Vladyslav Bankovskyi <vladyslav.bankovskyi@gmail.com>
2025-07-20 01:41:36 +0200

    first

---
+++ src/main/java/net/dregoscat/bettergraphics/BetterGraphics.java
@@ -0,0 +1,17 @@
+package net.dregoscat.bettergraphics;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterGraphics implements ModInitializer {
+	public static final String MOD_ID = "better-graphics";
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dregoscat/bettergraphics/BetterGraphicsClient.java
@@ -0,0 +1,10 @@
+package net.dregoscat.bettergraphics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterGraphicsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
